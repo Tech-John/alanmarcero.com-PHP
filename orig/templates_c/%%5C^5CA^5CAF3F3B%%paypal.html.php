674eea2e473ead8e8b40a5a2082d143df31887c8
Alan Marcero <alanmarcero@gmail.com>
2014-07-30 11:47:35 -0400

    remove cached files

--- orig/templates_c/%%5C^5CA^5CAF3F3B%%paypal.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.22, created on 2014-07-27 08:52:02
-         compiled from /home/content/71/7128071/html//templates/paypal.html */ ?>
-<h2>Thank you for your donation.</h2>
-<p>You will soon receive an email with your Login and Password.  Delivery can take up to twenty minutes. <strong>If you have any issues, or don't want to wait, simply re-purchase the item(s) at a price of "0.00".</strong></p>
-
-<p>Your donation encourages me to make my patches available through this website.  Thank you, again!</p>
