5301b0f6dd80aaa776153fe85e1ae5a31fe1b99f
Alan Marcero <alanmarcero@gmail.com>
2014-08-07 14:26:50 -0400

    more model methods

--- www/application/models/store_m.php
+++ www/application/models/store_m.php
@@ -7,7 +7,12 @@ class Store_m extends CI_Model
      * @var array
      */
     private $tbl = array(
-        'store' => 'store_entries'
+        'store' => 'store_entries',
+        'customers' => 'customers',
+        'purchases' => 'purchased_items',
+        'promo_emails' => 'promotional_emails',
+        'admins' => 'administrators',
+        'ebay' => 'auction_items'
     );
 
     /**
@@ -19,24 +24,38 @@ class Store_m extends CI_Model
     }
 
     /**
+     * [getCustomerCount returns the total count of customers is the customers table]
+     * @return [type] [description]
+     */
+    public function getCustomerCount()
+    {
+        return $this->db->count_all($this->tbl['customers']);
+    }
+
+    public function getPurchasesCount()
+    {
+        return $this->db->count_all($this->tbl['purchases']);
+    }
+
+    /**
      * [getStoreEntries returns all store entry data or data for the input item id]
      * @param  [type] $id [item id to return, else return all]
      * @return [type]     [description]
      */
     public function getStoreEntries($id = null)
     {
-        $this->db->from($this->tbl['store']);
+        $query = "SELECT * from {$this->tbl['store']} ";
 
-        # specified an id?
+        # specified an ID?
         if (!empty($id)) {
-            $this->db->where('id', $id);
+            $query .= " WHERE id = {$id} ";
         }
 
         # order by
-        $this->db->order_by('id', 'desc');
+        $query .= " ORDER BY id DESC";
 
         # grab data
-        $query = $this->db->get();
+        $query = $this->db->get($query);
         return $query->result_object();
     }
 }
