b2f61686ed75f8f9a4b801b71183473587bb90b2
Alan Marcero <alanmarcero@gmail.com>
2014-08-14 15:55:05 -0400

    only print out purchased cart if we have a cart

--- www/application/views/purchase_confirm.php
+++ www/application/views/purchase_confirm.php
@@ -1,12 +1,18 @@
-<h2>Thank you for purchasing the following items:</h2>
-<ul>
 <?php
-    foreach ($cart as $id => $item) {
-        echo "<li>" . $item['name'] . "</li>";
+    # only print cart info if we have a cart
+    if (!empty($cart)) {
+        echo "<h2>Thank you for purchasing the following items:</h2>";
+        echo "<ul>";
+
+        # print out each item
+        foreach ($cart as $id => $item) {
+            echo "<li>" . $item['name'] . "</li>";
+        }
+
+        echo "</ul>";
+        echo "<hr />";
+        echo "Total Paid: $0.00";
     }
 ?>
-</ul>
-<hr />
-Total Paid: $0.00
 <p>&nbsp;</p>
 <strong>You may download your items in the <a href="/customers">Customer's Section</a></strong>
