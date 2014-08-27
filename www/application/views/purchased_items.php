3eeb2aceaeb40bfa805f1e067aee272b25c9772b
Alan Marcero <alanmarcero@gmail.com>
2014-08-27 09:47:26 -0400

    verbiage

--- www/application/views/purchased_items.php
+++ www/application/views/purchased_items.php
@@ -1,4 +1,4 @@
-<p>If you encounter a problem while downloading an item, you may contact me at <a href="mailto:<?php echo ADMIN_EMAIL ?>"><?php echo ADMIN_EMAIL ?></a>.</p>
+<p>If you have any questions, you may contact me at <a href="mailto:<?php echo ADMIN_EMAIL; ?>"><?php echo ADMIN_EMAIL; ?></a>.</p>
 
 <?php
     foreach ($items as $item) {
